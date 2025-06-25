0a0ca8401f30c49eea4b39459091abc94f837ea4
YERAN0105 <yeranfernando2002@gmail.com>
2025-06-25 16:54:37 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -67,6 +67,6 @@ def predict():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=False, host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
 
